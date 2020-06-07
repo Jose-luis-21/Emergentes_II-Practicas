4fa39a13d419ff6daa820285d0cad089f6cbbb6d
Jose Luis <54046238+Jose-luis-21@users.noreply.github.com>
2020-06-07 19:13:18 -0400

    Update productos.java

--- productos.java
+++ productos.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.emergentes;
 
 import java.io.IOException;
@@ -13,10 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Alex
- */
+
 @WebServlet(name = "productos", urlPatterns = {"/productos"})
 public class productos extends HttpServlet {
     @Override
